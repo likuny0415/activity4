46ccd6fbbad6e6678f733ec9b94e48a899bb07d8
likuny0415 <li.kuny@husky.neu.edu>
2023-09-18 12:49:40 -0400

    activity4: update

--- src/README.md
+++ src/README.md
@@ -2,3 +2,6 @@
 
 ## Contact
 Kunyang Li, li.kuny@northeastern.edu
+Jae Wook Kim, kim.jaew@northeastern.edu
+Dezeng Kong, kong.dez@northeastern.edu
+Tracy Qiu, qiu.tr@northeastern.edu
--- src/WeatherDataObserver.ts
+++ src/WeatherDataObserver.ts
@@ -1,7 +1,9 @@
 import IWeatherDataClient from "./WeatherDataClient"
 
 export default interface IWeatherDataObserver {
+    // update the new temperature
     update():void
 
+    //  adds consumer
     addListener(listener:IWeatherDataClient): void
 }
